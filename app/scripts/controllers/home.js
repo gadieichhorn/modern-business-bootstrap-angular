d63ced5fefc24f85252c5769c4233e45e9b3401d
Gadi <Gadi@LenovoWin8>
2015-06-15 14:23:09 +0100

    UI carousel doesnt work for this design. need more time. rollback the changes

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -8,10 +8,7 @@
  * Controller of the modernbusinessApp
  */
 angular.module('mbApp')
-  .controller('HomeCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+        .controller('HomeCtrl', function ($scope) {
+
+
+        });
